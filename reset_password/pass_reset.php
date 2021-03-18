bbee8b2a5a19441dbca4056ec6330e2b3ca605f5
adi1234 <adi@20minutemail.it>
2021-03-18 19:22:28 +0530

    update new code

--- reset_password/pass_reset.php
+++ reset_password/pass_reset.php
@@ -98,94 +98,94 @@
                 }			
 			});	
 		});
-
-
-</script>
-<style type="text/css">
-body {
-    margin-top: 40px;
-    margin-bottom: 20px;
-}
-.container {
-    margin-right: 15px;
-    margin-left: 15px;
-}
-.form-control {
-    margin-bottom: 10px;
-}
-.success_msg {
-    font-size: 20px;
-    color: green;
-    text-align: center;
-    margin: 5px;
-}
-.error_msg {
-    font-size: 20px;
-    color: red;
-    text-align: center;
-    margin: 5px;
-}
-.change_password_sec .form-group {
-    margin-right: 0px;
-    margin-left: 0px;
-}
-.change_password_sec {
-    background-color: #ececec;
-    border: 1px solid #ececec;
-    border-radius: 10px;
-    /*box-shadow: 0 8px 6px -6px black;*/
-    box-shadow: 0 0 10px;
-    /*box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;*/
-}
-.reset_pass_sec {
-    margin-top: 40px;
-}
-.reset_pass_sec h1, .reset_pass_sec h5, .reset_pass_sec img {
-    text-align: center;
-}
-#resetPassword-error, #confPassword-error {
-    color: red;
-    font-weight: normal;
-    margin-bottom: 20px;
-}
-.logo_img {
-    text-align: center;
-}
-.laari_txt {
-    text-align: center;
-    margin-top: 30px;
-}
-.laari_txt p {
-    font-size: 25px;
-    color: blue;
-    font-weight: 600;
-}
-</style>
+	</script>
+	<style type="text/css">
+	body {
+	    margin-top: 40px;
+	    margin-bottom: 20px;
+	}
+	.container {
+	    margin-right: 15px;
+	    margin-left: 15px;
+	}
+	.form-control {
+	    margin-bottom: 10px;
+	}
+	.success_msg {
+	    font-size: 20px;
+	    color: green;
+	    text-align: center;
+	    margin: 5px;
+	}
+	.error_msg {
+	    font-size: 20px;
+	    color: red;
+	    text-align: center;
+	    margin: 5px;
+	}
+	.change_password_sec .form-group {
+	    margin-right: 0px;
+	    margin-left: 0px;
+	}
+	.change_password_sec {
+	    background-color: #ececec;
+	    border: 1px solid #ececec;
+	    border-radius: 10px;
+	    /*box-shadow: 0 8px 6px -6px black;*/
+	    box-shadow: 0 0 10px;
+	    /*box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;*/
+	}
+	.reset_pass_sec {
+	    margin-top: 40px;
+	}
+	.reset_pass_sec h1, .reset_pass_sec h5, .reset_pass_sec img {
+	    text-align: center;
+	}
+	#resetPassword-error, #confPassword-error {
+	    color: red;
+	    font-weight: normal;
+	    margin-bottom: 20px;
+	}
+	.logo_img {
+	    text-align: center;
+	}
+	.laari_txt {
+	    text-align: center;
+	    margin-top: 30px;
+	}
+	.laari_txt p {
+	    font-size: 25px;
+	    color: blue;
+	    font-weight: 600;
+	}
+	</style>
 </head>
+
 <body>
 	<div class="container">
 		<form class="form-horizontal" action="" name="myform" id="myform" method="post">
 			<div class="row">
-					<div class="col-xs-12 col-md-6 col-md-offset-4 change_password_sec">
-						<div class="form-group ">
-							<div class="reset_pass_sec" id="reset_pass_sec">	
-								<div class="logo_img">
-									<img src="laari_logo.png">
-								</div>
-								<h1 style="font-size: 30px; color: #1958E0;">Reset Password</h1>
-				          		<h5 style="font-size: 15px;">Please Enter New Password Below</h5>
-				          		<input type="password" class="form-control" id="resetPassword" name="resetPassword" placeholder="New Password">
-						        <input type="password" class="form-control" id="confPassword" name="confPassword" placeholder="Confirm Password">
-						    	<input type="submit" value="Reset Password" class="btn btn-primary btn-block submit_btn"/>
-						    </div>
-						    <div class="error_msg"></div>	
-						    <div class="success_msg"></div>
-						</div>
+				<div class="col-xs-12 col-md-6 col-md-offset-4 change_password_sec">
+					<div class="form-group ">
+						<div class="reset_pass_sec" id="reset_pass_sec">	
+							<div class="logo_img">
+								<img src="laari_logo.png">
+							</div>
+							<h1 style="font-size: 30px; color: #1958E0;">Reset Password</h1>
+				       		<h5 style="font-size: 15px;">Please Enter New Password Below</h5>
+				       		<input type="password" class="form-control" id="resetPassword" name="resetPassword" placeholder="New Password">
+					        <input type="password" class="form-control" id="confPassword" name="confPassword" placeholder="Confirm Password">
+					    	<input type="submit" value="Reset Password" class="btn btn-primary btn-block submit_btn"/>
+					    </div>
+					    <div class="error_msg"></div>	
+					    <div class="success_msg"></div>
 					</div>
+				</div>
 			</div>
 		</form>
 	</div>
 </body>
+
 </html>
 
 <!-- https://forum.jquery.com/topic/changing-password-validation-to-check-for-atleast-one-character-and-one-number -->
